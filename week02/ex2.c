d3fb2304e383e3a73f56c53166b3d6815f457c3b
vboxuser <vboxuser@Ubuntu01.myguest.virtualbox.org>
2023-09-14 23:28:15 +0300

    task 2

---
+++ week02/ex2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char s[200];
+    char temp;
+    int len = 0;
+    while (1) {
+        temp = getchar();
+        if (temp == '\n' || temp == '.') {
+            break;
+        }
+        s[len++] = temp;
+    }
+    printf("\"");
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+    printf("\"\n");
+    return 0;
+}
