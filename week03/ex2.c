b2e646265649ba48e582465116842874ef8a76ac
Egor Meganov <53008072+RwKaLs@users.noreply.github.com>
2023-09-24 23:30:47 +0300

    Update ex2.c

--- week03/ex2.c
+++ week03/ex2.c
@@ -16,7 +16,7 @@ double area(Point A, Point B, Point C) {
 int main() {
     Point A, B, C;
     A.x = 2.5, A.y = 6;
-    B.x = 1, B.y =2.2;
+    B.x = 1, B.y = 2.2;
     C.x = 10, C.y = 6;
     printf("%f\n", area(A, B, C));
     printf("%f\n", distance(A, B));
