b5da98c1b8f434b5b6fab802d0bc49e9cbda1a41
Egor Meganov <53008072+RwKaLs@users.noreply.github.com>
2023-11-20 19:34:37 +0300

    Update ex2.sh

--- week11/ex2.sh
+++ week11/ex2.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Compile the create_fs.c program
 gcc create_fs.c -o create_fs
-
-# Create the file system
 ./create_fs disk0
 
-# Compile the ex2.c program
 gcc ex2.c -o ex2
-
-# Process the input from the input file
 ./ex2 disk0 input.txt
 
 rm create_fs
